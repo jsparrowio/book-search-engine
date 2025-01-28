16974e15a635f673377321dd5a3c228c7c856004
jsparrowio <jsparrowio@outlook.com>
2025-01-27 16:56:16 -0800

    another file change for render

--- server/src/server.ts
+++ server/src/server.ts
@@ -37,11 +37,11 @@ const startApolloServer = async () => {
   // set up production mode
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../../client/build')));
 
 
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
 
